8d2b3c2673cc6b038f87ecf3e39e10f60051867e
willherzog <willherzog@gmail.com>
2023-06-27 12:40:40 -0600

    Update ToggleSwitchType.php
    Add HTML class "hidden-widget" to the checkbox input for toggle switches

--- src/Form/Type/ToggleSwitchType.php
+++ src/Form/Type/ToggleSwitchType.php
@@ -3,6 +3,7 @@
 namespace WHSymfony\WHFormBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\{FormInterface,FormView};
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 /**
@@ -15,5 +16,16 @@ class ToggleSwitchType extends AbstractType
 	public function getParent(): string
 	{
 		return CheckboxType::class;
+	}
+
+	public function buildView(FormView $view, FormInterface $form, array $options): void
+	{
+		$htmlClass = 'hidden-widget';
+
+		if( !isset($view->vars['attr']['class']) ) {
+			$view->vars['attr']['class'] = $htmlClass;
+		} elseif( $view->vars['attr']['class'] !== $htmlClass && is_string($view->vars['attr']['class']) && !str_contains($view->vars['attr']['class'], $htmlClass) ) {
+			$view->vars['attr']['class'] .= ' '. $htmlClass;
+		}
 	}
 }

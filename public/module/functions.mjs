dfbba92eda1040a107aeec3d48491e7ec5574705
willherzog <willherzog@gmail.com>
2023-07-27 15:51:18 -0600

    Update functions.mjs
    Create global variable instead since it needs to work as both an object and as a function

--- public/module/functions.mjs
+++ public/module/functions.mjs
@@ -11,9 +11,9 @@
  */
 
 // jQuery needs to be available, either as the global variable "$"" or the global variable "jQuery";
-// if only the latter is available, a new variable is created here with the same name as the former.
+// if only the latter is available, a new global variable is created with the same name as the former.
 if( typeof $ === 'undefined' && typeof jQuery === 'object' ) {
-	var $ = jQuery;
+	window.$ = jQuery;
 }
 
 const haveTranslator = typeof Translator === 'object' && typeof Translator.trans === 'function';
